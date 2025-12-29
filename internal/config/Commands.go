e2b86c0797e8dabc459708ad603d6eee28a300c9
jcfullmer <claye@fullmer.pro>
2025-12-29 01:47:36 -0500

    added the abililty to login (set username in config file)

--- internal/config/Commands.go
+++ internal/config/Commands.go
@@ -1,21 +1,75 @@
 package Config
 
 import (
-	"errors"
+	"fmt"
+	"os"
 )
 
 type State struct {
 	config *Config
 }
 
-type command struct {
-	name string
-	args []string
+type Command struct {
+	Name string
+	Args []string
 }
 
-```func handlerLogin(s *State, cmd command) error {
-	if len(cmd.args) == 0 {
-		return errors.New("Please input an argument for the command.")
+type Commands struct {
+	Handlers map[string]func(*State, Command) error
+}
+
+func GetState() (*State, error) {
+	config, err := Read()
+	if err != nil {
+		return &State{}, err
+	}
+	return &State{
+		&config,
+	}, nil
+}
+
+func CreateCommand(args []string) Command {
+	name := args[1]
+	var argies []string
+	if len(args) >= 3 {
+		argies = args[2:]
+	}
+	return Command{
+		Name: name,
+		Args: argies,
 	}
 }
-```
+
+func (c *Commands) Run(s *State, cmd Command) error {
+	if _, ok := c.Handlers[cmd.Name]; !ok {
+		return fmt.Errorf("command not found")
+	}
+	err := c.Handlers[cmd.Name](s, cmd)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Commands) Register(name string, f func(*State, Command) error) error {
+	if _, ok := c.Handlers[name]; ok {
+		return fmt.Errorf("command already exists")
+	}
+	c.Handlers[name] = f
+	return nil
+}
+
+func HandlerLogin(s *State, cmd Command) error {
+	if len(cmd.Args) == 0 {
+		fmt.Print("a username is required")
+		os.Exit(1)
+		return fmt.Errorf("Please input a username to login.")
+	}
+	newName := cmd.Args[0]
+	err := s.config.SetUser(newName)
+	if err != nil {
+		return err
+	}
+	fmt.Print("The user has been set.")
+	return nil
+}
--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	Config "github.com/jcfullmer/gatoRSS/internal/config"
 )
 
 func main() {
-	config, _ := Config.Read()
-	config.SetUser("Claye")
-	config, _ = Config.Read()
-	fmt.Println(config)
+	configState, _ := Config.GetState()
+	cmds := &Config.Commands{
+		Handlers: map[string]func(*Config.State, Config.Command) error{
+			"login": Config.HandlerLogin,
+		},
+	}
+	if len(os.Args) < 2 {
+		fmt.Print("No args")
+		os.Exit(1)
+	}
+	argInput := os.Args
+
+	cmd := Config.CreateCommand(argInput)
+	fmt.Println(cmd.Name)
+	err := cmds.Run(configState, cmd)
+	if err != nil {
+		fmt.Print(err)
+	}
 }
